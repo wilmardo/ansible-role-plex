d08f9fa9db847d3bb343fe4e8aee0bbffa52c53e
wilmardo <wilmar94do@hotmail.com>
2017-09-03 19:14:25 +0200

    Added new deploy client and secure var to .travis.yml

--- deploy/client.sh
+++ deploy/client.sh
@@ -1,2 +1,21 @@
-#!/bin/sh
-echo $1 | ncat --ssl --ssl-verify travisci.lansible.nl
+#!/bin/bash
+url=$1
+role=$(printenv TRAVIS_REPO_SLUG | rev | cut -d "-" -f1 | rev) #reverse cut leaves last part of repo name which is role name
+
+echo "Received url: $url"
+echo "Received role: $role"
+exec 3>&1
+if [ "$DEPLOY_CREDENTIALS" ]; then
+    echo "DEPLOY_CREDENTIALS defined, sending basic auth request"
+    http_status=$(curl -u $DEPLOY_CREDENTIALS -v -s -w "%{http_code}" -o >(cat >&3) $url/?$role)
+else
+    echo "DEPLOY_CREDENTIALS undefined, sending request"
+    http_status=$(curl -v -s -w "%{http_code}" -o >(cat >&3) $url/?$role)
+fi
+
+echo "HTTP status: $http_status"
+if [[ $http_status == 200 ]]; then
+    exit 0
+else
+    exit 1
+fi
